fa124126bad7e3c8646aea302cd5c4c68279de7a
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-03-26 11:52:05 +0530

    added components and working with props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact'
+import Header from './components/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <Header branding = "Contact Manager"/>
+        <Contact 
+        name = "Tarun Manchukonda"
+        email ="xxx@gmail.com" 
+        phone= "987654321" 
+        />
+        <Contact 
+        name = "Mounika Sharma"
+        email ="ms@gmail.com" 
+        phone= "986532147" 
+        />
       </div>
     );
   }
