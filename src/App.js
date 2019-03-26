8b3fcf17b471e9c1f915a4d47f8212c18367dcd3
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-03-26 14:59:02 +0530

    bootstrap added to navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact'
-import Header from './components/Header'
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 class App extends Component {
