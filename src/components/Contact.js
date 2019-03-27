50620b9edb75d4ad830db49db646fb7d2c263e76
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-03-27 11:47:23 +0530

    updated component state in contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,26 +3,33 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = (e) => {
-    console.log(e.target);
+  state = {
+    showContactInfo: false
   };
 
   render() {
       const { name,email,phone } = this.props.contact;
+      const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
-        {name}
+        {name} {''} 
         <i 
-          onClick= {this.onShowClick} 
+        onClick= {() => 
+        this.setState({showContactInfo:
+        !this.state.showContactInfo })
+      } 
           className="fas fa-sort-down" />
         </h4>
+        {showContactInfo ? (
         <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
+            <li className="list-group-item">Email: 
+            {email}</li>
+            <li className="list-group-item">Phone: 
+            {phone}</li>
         </ul>
+        ) : null}
       </div>
     );
   }
