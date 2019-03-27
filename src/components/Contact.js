eef9fe8321b778fc98a88c1821c192e767cfe4f2
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-03-27 09:45:24 +0530

    added events 7

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+  state = {};
+
+  onShowClick = (e) => {
+    console.log(e.target);
+  };
+
   render() {
-      const { contact } = this.props;
+      const { name,email,phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
+        <h4>
+        {name}
+        <i 
+          onClick= {this.onShowClick} 
+          className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
-            <li className="list-group-item">Email: {contact.email}</li>
-            <li className="list-group-item">Phone: {contact.phone}</li>
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
